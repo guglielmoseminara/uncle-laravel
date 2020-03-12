e63b63cae9f18dca23d133438278b18585cd83dd
Fabrizio Pulvirenti <fabrizio.pulvirenti.86@gmail.com>
2020-03-12 14:50:39 +0100

    put router service provider for routing resources

---
+++ src/Providers/RouteServiceProvider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace UncleProject\UncleLaravel\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use App;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapApiRoutes();
+
+        $this->mapWebRoutes();
+
+        //
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::group([
+            'middleware' => 'api',
+            'namespace' => $this->namespace,
+            'prefix' => 'api',
+        ], function ($router) {
+            foreach(config('uncle.resources') as $resource => $classPath) {
+                $filePath = base_path("app/Http/Resources/$resource/$resource"."Routes.php");
+                if (file_exists($filePath)) {
+                    require_once $filePath;
+                }
+            }
+        });
+    }
+}
