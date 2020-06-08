32e6b19b21ba437b6397b836ef00b9792d554e03
Fabrizio Pulvirenti <fabrizio.pulvirenti.86@gmail.com>
2020-06-09 00:11:25 +0200

    check xml exists

--- src/XMLCore/XMLRoutes.php
+++ src/XMLCore/XMLRoutes.php
@@ -8,17 +8,18 @@ use App;
 
 $api = app(Router::class);
 $xml = App::make('XMLResource');
-$routes = $xml->getResourceRoutes();
-$routeRevolver = new XMLRouteResolver();
 
-foreach ($routes as $resourceRoutes)
-{
-    foreach ($resourceRoutes as $versionRoutes){
-        $api->version($versionRoutes->attributes()['v']->__toString(), function ($api) use ($versionRoutes, $routeRevolver){
+if($xml->hasXML()){
+    $routes = $xml->getResourceRoutes();
+    $routeRevolver = new XMLRouteResolver();
 
-            $routeRevolver->createRoutes($api, $versionRoutes);
-
-        });
+    foreach ($routes as $resourceRoutes)
+    {
+        foreach ($resourceRoutes as $versionRoutes){
+            $api->version($versionRoutes->attributes()['v']->__toString(), function ($api) use ($versionRoutes, $routeRevolver){
+                $routeRevolver->createRoutes($api, $versionRoutes);
+            });
+        }
     }
 }
 
